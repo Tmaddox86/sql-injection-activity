1badedfa65eec81d2295b8029c68b4f37a71c2e4
Tara Maddox <TMaddox85@gmail.com>
2023-05-09 21:47:18 -0700

    added sqlite3 in app.js

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ const app = express();
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+const db = new sqlite3.Database(':memory:');
+db.serialize(function () {
+ db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+ db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+});
